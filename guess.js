db1fb9c4a2bf34e719bc8257c2c892d7f2ec973e
dahalh <dahalh@outlook.com>
2022-02-25 16:27:23 +1100

    correct message displayed

--- guess.js
+++ guess.js
@@ -1,15 +1,41 @@
 const numElm = document.getElementById("number");
 const btnElm = document.getElementById("guess-btn");
+const displayElm = document.getElementById("display-graphic");
+const lowerElm = document.getElementById("lower-display");
+const higherElm = document.getElementById("higher-display");
 const correctNum = 12;
 
 
-btnElm.addEventListener("click", () => {
+const handleOnClick = () => { btnElm.addEventListener("click", () => {
     console.log("clicked", numElm.value);
+    let str = "";
 
-    // if(correctNum == numElm.value){
-    //     alert(`You are correct! The correct is ${correctNum}` );
-    // }
-    // else{
-    //     alert(`Incorrect!`);
-    // };
+    if(correctNum == numElm.value){
+        // alert(`You are correct! The correct is ${correctNum}` );
+        str += `
+        <img src="correct.jpg" alt="correct" height="300" width="300">
+        <div class="d-flex justify-content-center">
+            CORRECT
+        </div>
+        `;
+    }
+    else if (numElm.value > correctNum){
+        str += `
+        <img src="down.jpg" alt="lower" height="300">
+        <div class="d-flex justify-content-center">
+            LOWER
+        </div>
+        `;
+    } else if (numElm.value < correctNum){
+        str += `
+        <img src="up.jpg" alt="higher" height="300" width="300">
+        <div class="d-flex justify-content-center">
+            HIGHER
+        </div>
+        `;
+    }
+    
+    displayElm.innerHTML = str;
 });
+
+};
